6cf4ab8328b760ebab9a3094be4d6f148dea8f54
Konstantin <constantinopladwar@gmail.com>
2023-04-16 12:24:50 +0200

    8. Cree Clase FuenteDatosFicheros, implementé patron singlinton y cambie visilidad a paquete de metodo crear en clase FactoriaFuenteDatos.

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/ficheros/Alquileres.java
@@ -0,0 +1,260 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.ficheros;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres;
+
+public class Alquileres implements IAlquileres {
+
+	private List<Alquiler> coleccionAlquileres  = new ArrayList<>();
+	
+	//instancia
+	private static Alquileres instancia = new Alquileres();
+
+	//constructor privado
+	private Alquileres() {
+		
+	}
+	
+	//obtener instancia
+	public static IAlquileres getInstancia() {
+
+		return instancia;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#get()
+	 */
+	
+	@Override
+	public List<Alquiler> get() {
+
+		List<Alquiler> alquileresCopia = new ArrayList<>(coleccionAlquileres);
+		return alquileresCopia;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#get(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente)
+	 */
+	
+	@Override
+	public List<Alquiler> get(Cliente cliente) {
+
+		//almaceno los alquileres asociados con cliente recibido
+		List<Alquiler> listaAlquileres = new ArrayList<>();
+		//creo un iterador para la colección de alquileres.
+		Iterator<Alquiler> iterator = coleccionAlquileres.iterator();
+		
+		//se ejecuta mientras la colecciónAlquileres tenga elementos que no iterados
+		while (iterator.hasNext()) {
+			//iterador avanza al siguiente elemento y se almacena en variable alquiler
+			Alquiler alquiler = iterator.next();
+			//se comprueba si cliente recibido es igual al cliente de alquiler 
+			if (alquiler.getCliente().equals(cliente)) {
+				
+				//si es igual este alquiler se añade a la lista de alquileres con este cliente asosiado
+				listaAlquileres.add(alquiler);
+			}
+		}
+		
+		//devolvemos la lista de alquileres asociada a cliente
+		return listaAlquileres;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#get(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo)
+	 */
+	
+	@Override
+	public List<Alquiler> get(Vehiculo turismo) {
+
+		//almaceno los alquileres asociados con turismo recibido
+		List<Alquiler> listaAlquileres = new ArrayList<>();
+		//creo un iterador para la colección de alquileres.
+		Iterator<Alquiler> iterador = coleccionAlquileres.iterator();
+		
+		//se ejecuta mientras la colecciónAlquileres tenga elementos que no iterados
+		while (iterador.hasNext()) {
+			//iterador avanza al siguiente elemento y se almacena en variable alquiler
+			Alquiler alquiler = iterador.next();
+			
+			//se comprueba si turismo recibido es igual al turismo de alquiler 
+			if (alquiler.getVehiculo().equals(turismo)) {
+				
+				//si es igual este alquiler se añade a la lista de alquileres con este turismo asosiado
+				listaAlquileres.add(alquiler);
+			}
+		}
+		//devolvemos la lista de alquileres asociada a turismo
+		return listaAlquileres;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#getCantidad()
+	 */
+	
+	@Override
+	public int getCantidad() {
+
+		return coleccionAlquileres.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#insertar(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler)
+	 */
+	
+	@Override
+	public void insertar(Alquiler alquiler) throws OperationNotSupportedException {
+
+		if (alquiler == null) {
+			throw new NullPointerException("ERROR: No se puede insertar un alquiler nulo.");
+		}
+
+		comprobarAlquiler(alquiler.getCliente(), alquiler.getVehiculo(), alquiler.getFechaAlquiler());
+		coleccionAlquileres.add(alquiler);
+
+	}
+
+	private void comprobarAlquiler(Cliente cliente, Vehiculo turismo, LocalDate fechaAlquiler)
+			throws OperationNotSupportedException {
+
+		for (Iterator<Alquiler> iterator = coleccionAlquileres.iterator(); iterator.hasNext();) {
+			Alquiler alquiler = iterator.next();
+
+			if (alquiler.getFechaDevolucion() == null) {
+				if (alquiler.getCliente().equals(cliente)) {
+					throw new OperationNotSupportedException("ERROR: El cliente tiene otro alquiler sin devolver.");
+				}
+				if (alquiler.getVehiculo().equals(turismo)) {
+					throw new OperationNotSupportedException("ERROR: El turismo está actualmente alquilado.");
+				}
+			} else {
+				if (alquiler.getCliente().equals(cliente) && (alquiler.getFechaDevolucion().isAfter(fechaAlquiler)
+						|| alquiler.getFechaDevolucion().isEqual(fechaAlquiler))) {
+					throw new OperationNotSupportedException("ERROR: El cliente tiene un alquiler posterior.");
+				}
+				if (alquiler.getVehiculo().equals(turismo) && (alquiler.getFechaDevolucion().isAfter(fechaAlquiler)
+						|| alquiler.getFechaDevolucion().isEqual(fechaAlquiler))) {
+					throw new OperationNotSupportedException("ERROR: El turismo tiene un alquiler posterior.");
+				}
+			}
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#devolver(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler, java.time.LocalDate)
+	 */
+
+	public void devolver(Cliente cliente, LocalDate fechaDevolucion)
+			throws NullPointerException, OperationNotSupportedException {
+		if (cliente == null || fechaDevolucion == null) {
+			throw new NullPointerException("ERROR: No se puede devolver un alquiler nulo.");
+		}
+		Alquiler alquiler = null;
+		alquiler = getAlquilerAbierto(cliente);
+		if (alquiler == null) {
+			throw new OperationNotSupportedException("ERROR: No existe ningún alquiler igual.");
+		}
+		alquiler.devolver(fechaDevolucion);
+	}
+
+	public void devolver(Vehiculo vehiculo, LocalDate fechaDevolucion)
+			throws NullPointerException, OperationNotSupportedException {
+		if (vehiculo == null || fechaDevolucion == null) {
+			throw new NullPointerException("ERROR: No se puede devolver un alquiler nulo.");
+		}
+		Alquiler alquiler = null;
+		alquiler = getAlquilerAbierto(vehiculo);
+		if (alquiler == null) {
+			throw new OperationNotSupportedException("ERROR: No existe ningún alquiler igual.");
+		}
+		alquiler.devolver(fechaDevolucion);
+	}
+
+	private Alquiler getAlquilerAbierto(Cliente cliente) {
+		if (cliente == null) {
+			throw new NullPointerException("ERROR: No se puede buscar un alquiler de cliente nulo.");
+		}
+		Alquiler alquiler = null;
+		for (Alquiler alquilerBusca : coleccionAlquileres) {
+			if (alquilerBusca.getFechaDevolucion() == null && alquilerBusca.getCliente().equals(cliente)) {
+				alquiler = alquilerBusca;
+			}
+
+		}
+		return alquiler;
+	}
+
+	private Alquiler getAlquilerAbierto(Vehiculo vehiculo) {
+		if (vehiculo == null) {
+			throw new NullPointerException("ERROR: No se puede buscar un alquiler de cliente nulo.");
+		}
+		Alquiler alquiler = null;
+		for (Alquiler alquilerBusca : coleccionAlquileres) {
+			if (alquilerBusca.getFechaDevolucion() == null && alquilerBusca.getVehiculo().equals(vehiculo)) {
+				alquiler = alquilerBusca;
+			}
+
+		}
+		return alquiler;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#borrar(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler)
+	 */
+	
+	@Override
+	public void borrar(Alquiler alquiler) throws OperationNotSupportedException {
+
+		if (alquiler == null) {
+
+			throw new NullPointerException("ERROR: No se puede borrar un alquiler nulo.");
+		}
+
+		if (!coleccionAlquileres.contains(alquiler)) {
+
+			throw new OperationNotSupportedException("ERROR: No existe ningún alquiler igual.");
+		} else {
+			coleccionAlquileres.remove(alquiler);
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IAlquileres#buscar(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler)
+	 */
+	
+	@Override
+	public Alquiler buscar(Alquiler alquiler) {
+		if (alquiler == null) {
+			throw new NullPointerException("ERROR: No se puede buscar un alquiler nulo.");
+		}
+
+		int indice = coleccionAlquileres.indexOf(alquiler);
+
+		if (indice == -1) {
+			return null;
+		} else {
+			return coleccionAlquileres.get(coleccionAlquileres.indexOf(alquiler));
+		}
+	}
+
+}
---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/ficheros/Vehiculos.java
@@ -0,0 +1,121 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.ficheros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IVehiculos;
+
+public class Vehiculos implements IVehiculos {
+
+
+	private List<Vehiculo> coleccionVehiculos = new ArrayList<>();
+	
+	//instancia
+	private static Vehiculos instancia = new Vehiculos();
+
+	private Vehiculos() {
+
+		
+	}
+	
+	//obtener instancia
+	public static IVehiculos getInstancia() {
+
+		return instancia;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IVehiculos#get()
+	 */
+	
+	@Override
+	public List<Vehiculo> get() {
+
+		List<Vehiculo> turismosCopia = new ArrayList<>(coleccionVehiculos);
+		return turismosCopia;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IVehiculos#getCantidad()
+	 */
+	
+	@Override
+	public int getCantidad() {
+
+		return coleccionVehiculos.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IVehiculos#insertar(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo)
+	 */
+	
+	@Override
+	public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
+
+		if (vehiculo == null) {
+
+			throw new NullPointerException("ERROR: No se puede insertar un vehiculo nulo.");
+		}
+
+		if (!coleccionVehiculos.contains(vehiculo)) {
+			coleccionVehiculos.add(vehiculo);
+		} else {
+
+			throw new OperationNotSupportedException("ERROR: Ya existe un vehiculo con esa matrícula.");
+
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IVehiculos#buscar(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo)
+	 */
+	
+	@Override
+	public Vehiculo buscar(Vehiculo vehiculo) {
+
+		if (vehiculo == null) {
+	        throw new NullPointerException("ERROR: No se puede buscar un vehiculo nulo.");
+	    }
+	    if (coleccionVehiculos.contains(vehiculo)) {
+	        return coleccionVehiculos.get(coleccionVehiculos.indexOf(vehiculo));
+	    } else {
+	        return null;
+	    }
+
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IVehiculos#borrar(org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Vehiculo)
+	 */
+	
+	@Override
+	public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
+
+		if (vehiculo == null) {
+
+			throw new NullPointerException("ERROR: No se puede borrar un vehiculo nulo.");
+		}
+
+		if (!coleccionVehiculos.contains(vehiculo)) {
+
+			throw new OperationNotSupportedException("ERROR: No existe ningún vehiculo con esa matrícula.");
+		} else {
+			coleccionVehiculos.remove(vehiculo);
+		}
+
+	}
+}
